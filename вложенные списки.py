4e3c411fd8537152f673d9371ac985f335262347
Artem <gandebobel@gmail.com>
2020-03-02 15:48:22 +0300

    Обратный обход списков

--- вложенные списки.py
+++ вложенные списки.py
@@ -38,8 +38,27 @@ print('Есле раскоментировать строчку "a[i][j] += 10",
 print(' ')
 print('Обход по индексам с выводом значений по столбцам. Берется сначала индекс 0 из каждого вложенного списка и '
       'выводится, на следующую строчку индекс 1 и так далее')
+print('for j in range(4):')
+print('    for i in range(3):')
+print('        print(a[i][j], end=' ')')
+print('    print()')
+print('print(a)')
 for j in range(4):
     for i in range(3):
         print(a[i][j], end=' ')
     print()
 print(a)
+print(' ')
+print('Обход по индексам вложенных списков в обратном порядке')
+print('for i in range(2, -1, -1):')
+print('    for j in range(3, -1, -1):')
+print('        print(a[i][j], end=' ')')
+print('    print()')
+print('print(a)')
+for i in range(2, -1, -1):
+    for j in range(3, -1, -1):
+        print(a[i][j], end=' ')
+    print()
+print(a)
+print(' ')
+
