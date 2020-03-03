441f2975e18b64c4630d541ddf52cff4678a5e1b
Artem <gandebobel@gmail.com>
2020-03-03 14:45:22 +0300

    Заполнение вложенного списка с клавиатуры

---
+++ Заполнение вложенного списка.py
@@ -0,0 +1,12 @@
+print('Заполнение вложенного списка с клавиатуры')
+a = []
+n = int(input())#stroka
+m = int(input())#stolbec
+
+for i in range(n):
+    b = []
+    for j in range(m):
+        b.append(int(input()))
+    a.append(b)
+for i in a:
+    print(i)
