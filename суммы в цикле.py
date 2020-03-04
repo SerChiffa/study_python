4c5f662dcd8892c1f46b4204581eedccf3f1fbab
Artem <gandebobel@gmail.com>
2020-03-04 18:10:52 +0300

    при условии 2 строк и подсчете суммы только 2 строки

--- суммы в цикле.py
+++ суммы в цикле.py
@@ -1,10 +1,17 @@
-#from sys import stdin
+from sys import stdin
 #print(sum(int(i) for i in stdin.readlines()[1].split()))
 
-line = input()
-symbols = line.split(' ')
-sums = 0
-for i in symbols:
-    digit = int(i)
-    sum += digit
-print(sums)
+#line = input()
+#symbols = line.split(' ')
+#sums = 0
+#for i in symbols:
+#    digit = int(i)
+#    sums += digit
+#print(sums)
+
+line1 = input()
+line2 = input().split()
+summa = 0
+for i in range(0, len(line2)):
+    summa += int(line2[i])
+print(summa)
