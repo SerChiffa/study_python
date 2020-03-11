73c1e573e838c6bbae11c3e698f68756e2bed8d7
Artem <gandebobel@gmail.com>
2020-03-11 22:35:22 +0300

    используя оператор сравнения

--- tusk4 minimumof two.py
+++ tusk4 minimumof two.py
@@ -1,7 +1,20 @@
-#n = int(input())
-#for i in range(1, n):
+# n = int(input())
+# for i in range(1, n):
 #    print(min(map(int, input().split())), end=' ')
 
-#или используя более правильно:
+# или используя более правильно:
 
-print(*(str(min(map(int, input().split()))) for _ in range(int(input()))), sep=' ')
+# print(*(str(min(map(int, input().split()))) for _ in range(int(input()))), sep=' ')
+
+# с использованием оператора сравнения
+
+n = int(input())
+a = ''
+for i in range(n):
+    l, r = list(input().split())
+    if int(l) < int(r):
+        a += l
+    else:
+        a += r
+    a += ' '
+print(a)
