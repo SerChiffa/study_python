9874136321d69f971a86d9e45e6edf6d36366326
Artem <gandebobel@gmail.com>
2020-03-11 16:25:03 +0300

    второй вариант - суммы чисел строк в виде строки без запятых через пробел

--- сумма из 2х столбцов.py
+++ сумма из 2х столбцов.py
@@ -1,9 +1,22 @@
-a = []
+#a = []
+#n = int(input())
+#for i in range(n):
+#    new_column = input().split()
+#    s = 0
+#    for j in range(0, len(new_column)):
+#        s += int(new_column[j])
+#    a.append(s)
+#print(a)
+
+#или таким образом, что даст список сумм без запятых
+
 n = int(input())
+a = ""
 for i in range(n):
-    new_column = input().split()
+    x = list(input().split())
     s = 0
-    for j in range(0, len(new_column)):
-        s += int(new_column[j])
-    a.append(s)
+    for j in x:
+        s += int(j)
+    a += str(s)
+    a += " "
 print(a)
