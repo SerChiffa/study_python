9b20045053a58ce0783559d4c7808df6568a7ee4
Artem <gandebobel@gmail.com>
2020-03-18 11:22:02 +0300

    Взвешенная сумма цифр

--- tusk13 weighted sum of digits.py
+++ tusk13 weighted sum of digits.py
@@ -1,3 +1,11 @@
+# n = int(input())
+# b = input()
+# print(*([sum(int(e)*i for i, e in enumerate(n, start=1)) for n in b.split()]))
+
 n = int(input())
-b = input()
-print(*([sum(int(e)*i for i, e in enumerate(n, start=1)) for n in b.split()]))
+s = input().split()
+for i in s:
+    wsd = 0
+    for j in range(len(i)):
+        wsd = wsd + int(i[j]) * (j + 1)
+    print(wsd, end=' ')
